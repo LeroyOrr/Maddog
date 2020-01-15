a30d2fc90679cbf72c1171120fc021007a0332f1
Leroy Orr <leroyorr@Leroys-MacBook-Pro-4.local>
2020-01-15 18:38:53 -0500

    Initial commit

---
+++ Maddog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maddog
+//
+//  Created by Leroy Orr on 1/15/20.
+//  Copyright © 2020 Leroy Orr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
